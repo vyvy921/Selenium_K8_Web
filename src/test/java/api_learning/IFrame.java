84997a8f86b69a449fe00ef2171433b3dc5b25ec
Vy Vy <vyvy.tester@gmail.com>
2022-07-23 18:41:16 +0700

    lesson 17 - iframe

---
+++ src/test/java/api_learning/IFrame.java
@@ -0,0 +1,47 @@
+package api_learning;
+
+import driver.DriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import url.Urls;
+
+public class IFrame implements Urls {
+    public static void main(String[] args) {
+
+        //Get chrome session
+        WebDriver driver = DriverFactory.getChromeDriver();
+
+        try {
+            //Navigate to target page
+            driver.get(baseUrl.concat(iframeSlug));
+
+            //locate the iframe
+            By iFrameSel = By.cssSelector("[id$='ifr']");
+            WebElement iFrameElem = driver.findElement(iFrameSel);
+
+            //Switch to the iframe
+            driver.switchTo().frame(iFrameElem);
+            driver.findElement(By.cssSelector("#tinymce p")).sendKeys("New texts");
+
+            // Locate element inside the iframe
+            WebElement editorInputElem = driver.findElement(By.id(("tinymce")));
+            editorInputElem.click();
+            editorInputElem.clear();
+            editorInputElem.sendKeys("input texttttttttt...");
+            Thread.sleep(1000);
+
+            // Switch back parent frame
+            driver.switchTo().defaultContent();
+            driver.findElement(By.linkText("Elemental Selenium")).click();
+            Thread.sleep(1000);
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        driver.quit();
+
+    }
+}
